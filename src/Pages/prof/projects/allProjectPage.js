99f29c3116e2b101249a7ae288927b44a37c7238
AnkitGurwan <ankitgurwan083@gmail.com>
2024-04-04 23:55:42 +0530

    student side completed

---
+++ src/Pages/prof/projects/allProjectPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Allprojects from './allProjectsComponent.js';
+import Header from '../../../components/mainPagesHeader.js';
+
+const AllProjectPage = () => {
+    return(
+        <div>
+            <Header/>
+            <Allprojects/>
+        </div>
+    )
+}
+
+export default AllProjectPage;;
---
+++ src/Pages/prof/projects/allReadMoreProjectCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Specificproject2 from './projectcardAllReadMore.js';
+import Header from '../../../components/mainPagesHeader.js';
+
+const AllReadMoreProjectCard = () => {
+    return(
+        <div>
+            <Header/>
+            <Specificproject2/>
+            
+        </div>
+    )
+}
+
+export default AllReadMoreProjectCard;
